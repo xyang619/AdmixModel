93ac5664265d2114298426252214b222bc169db3
Young <xyang619@gmail.com>
2014-10-19 16:54:35 +0200

    update prompt

--- src/AdmixModel.cpp
+++ src/AdmixModel.cpp
@@ -21,39 +21,44 @@ void help();
 int main(int argc, char **argv) {
 	//cout<<argc<<endl;
 
-	if ((argc < 2) || (argc < 12 && (string(argv[1]) != "-h" || string(argv[1])!="--help"))) {
-		cerr << "Need more arguments than provided, please check help again"
-				<< endl;
+	if (argc > 1 && (string(argv[1]) != "-h" || string(argv[1]) != "--help")) {
 		help();
 		exit(0);
 	}
+
+	if (argc < 12) {
+		cerr << "Need more arguments than provided, please check help again"
+				<< endl;
+		exit(1);
+	}
+
 	int i;
 	int mod = 1;
 	int gen = 1;
 	int ne = 1000;
 	double prop = 0.5;
-	double len = 0;
+	double len = 1;
 	int nsample = 10;
 	long seed = 0;
 	bool givenSeed = 0;
 
 	for (i = 0; i < argc; ++i) {
-		if (string(argv[i]) == "-h" || string(argv[i])=="--help") {
+		if (string(argv[i]) == "-h" || string(argv[i]) == "--help") {
 			help();
 			exit(0);
-		} else if (string(argv[i]) == "-g" || string(argv[i])=="--gen") {
+		} else if (string(argv[i]) == "-g" || string(argv[i]) == "--gen") {
 			gen = atoi(argv[++i]);
-		} else if (string(argv[i]) == "-p" || string(argv[i])=="--prop") {
+		} else if (string(argv[i]) == "-p" || string(argv[i]) == "--prop") {
 			prop = atof(argv[++i]);
-		} else if (string(argv[i]) == "-m" || string(argv[i])=="--model") {
+		} else if (string(argv[i]) == "-m" || string(argv[i]) == "--model") {
 			mod = atoi(argv[++i]);
-		} else if (string(argv[i]) == "-l" || string(argv[i])=="--len") {
+		} else if (string(argv[i]) == "-l" || string(argv[i]) == "--len") {
 			len = atof(argv[++i]);
-		} else if (string(argv[i]) == "-e" || string(argv[i])=="--ne") {
+		} else if (string(argv[i]) == "-e" || string(argv[i]) == "--ne") {
 			ne = atoi(argv[++i]);
-		} else if (string(argv[i]) == "-n" || string(argv[i])=="--samp") {
+		} else if (string(argv[i]) == "-n" || string(argv[i]) == "--samp") {
 			nsample = atoi(argv[++i]);
-		} else if (string(argv[i]) == "-s" || string(argv[i])=="--seed") {
+		} else if (string(argv[i]) == "-s" || string(argv[i]) == "--seed") {
 			seed = atol(argv[++i]);
 			givenSeed = 1;
 		}
@@ -73,9 +78,10 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 	if (!givenSeed) {
-		srand (time(NULL));} else {
-			srand(seed);
-		}
+		srand(time(NULL));
+	} else {
+		srand(seed);
+	}
 	Model model(mod, gen, ne, prop);
 	model.evolve(len);
 	vector<Chrom> sample = model.getPop().sample(nsample);
@@ -99,9 +105,11 @@ void help() {
 	cout << "	-h	--help	print help message" << endl;
 	cout << "	-g	--gen	generation since admixture" << endl;
 	cout << "	-p	--prop	ancestry proportion of admixed population" << endl;
-	cout << "	-m	--model	admixture model, 1 for HI, 2 for GA and 3 for CGF" << endl;
+	cout << "	-m	--model	admixture model, 1 for HI, 2 for GA and 3 for CGF"
+			<< endl;
 	cout << "	-l	--len	length of chromsome to simulate, unit in Morgan" << endl;
-	cout << "	-e 	--ne	effective population size for admixed population" << endl;
+	cout << "	-e 	--ne	effective population size for admixed population"
+			<< endl;
 	cout << "	-n	--samp	number of haplotypes to be sampled" << endl;
 	cout << "	-s	--seed	seed of random number generator" << endl;
 }
